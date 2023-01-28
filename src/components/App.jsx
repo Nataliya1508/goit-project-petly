166a3c72225b3b3df0686854e880492cd4db3d51
Lisnyi <russelhobbbs@gmail.com>
2023-01-29 00:18:39 +0200

    add Modal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,9 @@
 import { Box, Heading } from '@chakra-ui/react';
-import { Filter } from 'shared/components';
 
 export const App = () => {
   return (
-<>
     <Box display="flex" justifyContent="center" bg="accent.100">
       <Heading as="h1">React template with CHAKRA UI</Heading>
-      
     </Box>
-    <Filter>in good hands</Filter>
-
-</>
   );
 };

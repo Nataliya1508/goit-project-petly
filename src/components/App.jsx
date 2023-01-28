ea2b729b86832c596bccd95b318f72fb91c60d61
Lisnyi <russelhobbbs@gmail.com>
2023-01-28 23:30:10 +0200

    add custom button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,14 @@
 import { Box, Heading } from '@chakra-ui/react';
+import { Button } from 'shared/components';
 
 export const App = () => {
   return (
+<>
     <Box display="flex" justifyContent="center" bg="accent.100">
       <Heading as="h1">React template with CHAKRA UI</Heading>
+      
     </Box>
+    <Button>Cancel</Button>
+</>
   );
 };

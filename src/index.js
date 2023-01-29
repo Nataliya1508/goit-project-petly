2b6e8e6e2d90c78703348b2bb2bbdf04cef1463e
Velychko Daniil <101584452+Kl0filinj@users.noreply.github.com>
2023-01-29 10:57:59 +0100

    main routes | folders structure fixed

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import theme from 'utils/extendTheme';
 import { ChakraProvider } from '@chakra-ui/react';
 import { App } from 'components/App';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
+    <BrowserRouter basename="/goit-project-petly">
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );

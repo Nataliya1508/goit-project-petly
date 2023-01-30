e4b9fa7554d60d6baf6af85a439399373a506cdd
Velychko Daniil <101584452+Kl0filinj@users.noreply.github.com>
2023-01-30 18:06:23 +0100

    notices fix

--- src/components/NoticesCategoriesList/NoticesCategoriesList.jsx
+++ src/components/NoticesCategoriesList/NoticesCategoriesList.jsx
@@ -1,25 +1,27 @@
-import React from 'react'
+import React from 'react';
 
-import {NoticesCategoryItem} from './NoticesCategoryItem.js'
+import { NoticesCategoryItem } from './NoticesCategoryItem.js';
 
-const NoticesCategoriesList = (userPets) => {
-const filterPets = userPets.filter(pets => pets.condition === condition)
+const NoticesCategoriesList = userPets => {
+  // const filterPets = userPets.filter(pets => pets.condition === condition);
   return (
-    <ul key={filterPets.id}>
-      {filterPets.map(({id, favorite, avatar, title, breed, location, age, condition}) => (
-        <NoticesCategoryItem
-        key={id}
-        avatar = {avatar}
-        favorite = {favorite}
-        title = {title}
-        breed = {breed}
-        location = {location}
-        age = {age}
-        condition = {condition}
-        />
-      ))}
+    <ul key={userPets.id}>
+      {userPets.map(
+        ({ id, favorite, avatar, title, breed, location, age, condition }) => (
+          <NoticesCategoryItem
+            key={id}
+            avatar={avatar}
+            favorite={favorite}
+            title={title}
+            breed={breed}
+            location={location}
+            age={age}
+            condition={condition}
+          />
+        )
+      )}
     </ul>
-  )
-}
+  );
+};
 
-export default NoticesCategoriesList
+export default NoticesCategoriesList;

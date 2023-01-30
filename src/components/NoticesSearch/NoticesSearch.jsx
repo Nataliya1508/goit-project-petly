f68c583d037784306c057b66c62aa3078aacc27e
Velychko Daniil <101584452+Kl0filinj@users.noreply.github.com>
2023-01-30 17:57:05 +0100

    fix notices

--- src/components/NoticesSearch/NoticesSearch.jsx
+++ src/components/NoticesSearch/NoticesSearch.jsx
@@ -1,52 +1,59 @@
-import { useState} from 'react';
-import { Search, SearchForm, FormButton, FormInput } from "components/NoticesSearch/NoticesSearch"
-import PropTypes from "prop-types"
-import { CloseIcon } from '@chakra-ui/icon'
+import { useState } from 'react';
+// import {
+//   Search,
+//   SearchForm,
+//   FormButton,
+//   FormInput,
+// } from 'components/NoticesSearch/NoticesSearch';
+import PropTypes from 'prop-types';
+import { CloseIcon, Search2Icon } from '@chakra-ui/icons';
+import { Box } from '@chakra-ui/react';
 
-import SearchIcon from ""
-const NoticesSearch = (onSubmit) => {
-  const [query, setQuery] = useState("");
+const NoticesSearch = onSubmit => {
+  const [query, setQuery] = useState('');
   const [filteredData, setFilteredData] = useState([]);
 
-  const handleChange = (e) => {
+  const handleChange = e => {
     setQuery(e.target.value.toLowerCase());
     setFilteredData(e.target.value);
-  }
-  const handleSubmit = (e) => {
+  };
+  const handleSubmit = e => {
     e.preventDefault();
-    if(query.trim() === "") {
-      alert("What pet do you need?");
+    if (query.trim() === '') {
+      alert('What pet do you need?');
       return;
     }
     onSubmit(query);
-  }
+  };
 
   const clearInput = () => {
-    setFilteredData("");
-  }
+    setFilteredData('');
+  };
   console.log(filteredData);
   return (
-    <Search>
-      <SearchForm onSubmit={handleSubmit}>
-        <FormInput
-      type="text"
-      name="input"
-      autocomplete="off"
-      placeholder="Search"
-      onChange={handleChange}
-      value={filteredData}
-      />
-        <FormButton type="submit">
-{filteredData.length === 0 ?
-<SearchIcon /> : <CloseIcon onClick={clearInput} />
-}
-        </FormButton>
-      </SearchForm>
-    </Search>
-  )
-}
+    <Box>
+      <Box onSubmit={handleSubmit}>
+        <Box
+          type="text"
+          name="input"
+          autocomplete="off"
+          placeholder="Search"
+          onChange={handleChange}
+          value={filteredData}
+        />
+        <Box type="submit">
+          {filteredData.length === 0 ? (
+            <Search2Icon />
+          ) : (
+            <CloseIcon onClick={clearInput} />
+          )}
+        </Box>
+      </Box>
+    </Box>
+  );
+};
 NoticesSearch.propTypes = {
-  onSubmit: PropTypes.func.isRequired
-}
+  onSubmit: PropTypes.func.isRequired,
+};
 
 export default NoticesSearch;

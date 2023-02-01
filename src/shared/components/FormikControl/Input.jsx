617d1c11451cfb16454699c484a886bfba5bb37a
Lisnyi <russelhobbbs@gmail.com>
2023-02-01 21:41:48 +0200

    Update Input.jsx

--- src/shared/components/FormikControl/Input.jsx
+++ src/shared/components/FormikControl/Input.jsx
@@ -3,10 +3,14 @@ import {
   FormControl,
   FormLabel,
   Input,
-  FormErrorMessage, } from "@chakra-ui/react"
+  FormErrorMessage,
+  InputRightElement,
+  IconButton, 
+  InputGroup} from "@chakra-ui/react"
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons'
 import { Field } from 'formik';
 
-const CustomInput = ({label, name, id, mb='16px', ...rest}) => {
+const CustomInput = ({label, name, id, mb='16px', type='text', show, handleClick, ...rest}) => {
   return (
     <Box  w="240px" mb={mb}>
           <Field>
@@ -19,8 +23,10 @@ const CustomInput = ({label, name, id, mb='16px', ...rest}) => {
                               >{label}
                           </FormLabel>
                         : null}
+                <InputGroup>
                 <Field
                     as={Input}
+                    type={type}
                     id={id}
                     name={name}
                     variant='filled'
@@ -33,6 +39,17 @@ const CustomInput = ({label, name, id, mb='16px', ...rest}) => {
                     _placeholder={{ color: '#111111' }}
                     {...rest}
                   />
+                  {type === 'password' && <InputRightElement>
+                                            <IconButton
+                                              onClick={handleClick}
+                                              icon={show ? <ViewIcon/> : <ViewOffIcon/>}
+                                              width={'30px'}
+                                              height={'30px'}
+                                              minW={'none'}
+                                              backgroundColor={'transparent'}
+                                              borderRadius={'50%'}/>
+                                          </InputRightElement>}
+                </InputGroup>
                 <FormErrorMessage
                   fontSize="12px">
                     {form.errors[name]}

081f3f11a86b4c35c8185040936255b680053469
Lisnyi <russelhobbbs@gmail.com>
2023-02-01 20:03:15 +0200

    Update NoticesCategoryItem.jsx

--- src/components/NoticesCategoryItem/NoticesCategoryItem.jsx
+++ src/components/NoticesCategoryItem/NoticesCategoryItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import {Text} from '@chakra-ui/react'
+import {Text, Box, Image, Card, CardBody, Heading, CardFooter, Button} from '@chakra-ui/react'
+import { CardButton, FavoriteButton } from 'shared/components'
 
 const NoticesCategoryItem = (
   {
@@ -10,27 +11,74 @@ const NoticesCategoryItem = (
   breed,
   location,
   age,
+  price,
   condition,
   onDeletePets
   }) => {
   return (
-    <li>
-      <div>
-        <img src={avatar} alt="Pets" width="100" />
-        <p>{condition}</p>
-        <Text bg={ favorite ? "res.200" : "green.300"}></Text>
-      </div>
-    <h3>{title}</h3>
-      <p>Breed:{breed}</p> 
-      <p>Location:{location}</p>
-      <p>Age:{age}</p> 
-    <button>Learn more</button>
-    {favorite? <button
-      type='submit'
-      onClick={onDeletePets(id)}>
-      Delete
-    </button> : null}
-    </li>
+    <Card
+      as={'li'}
+      width={'100%'}
+      boxShadow={'7px 4px 14px rgba(49, 21, 4, 0.07)'}
+      borderBottomRadius={'20px'}
+      _notLast={{mb: '32px'}}>
+        <CardBody p={'0'} mb={(price || favorite) ? '0' : '30px'}>
+          <Box position={'relative'}>
+            <Image src={avatar} alt={breed} width={'100%'} height={'288px'}/>
+            <Box
+              as={'span'}
+              position={'absolute'}
+              top={'20px'}
+              display={'inline-flex'}
+              alignItems={'center'}
+              justifyContent={'center'}
+              w={'158px'}
+              h={'28px'}
+              px={'20px'}
+              py={'6px'}
+              borderRightRadius={'20px'}
+              color={'#111111'}
+              fontSize={'xs'}
+              fontWeight={'medium'}
+              lineHeight={'xl'}
+              letterSpacing={'0.04em'}
+              backgroundColor={'rgba(255, 255, 255, 0.6)'}
+              backdropFilter={'blur(2px)'}>
+                {condition}
+            </Box>
+            <FavoriteButton/>
+          </Box>
+          <Box p={'20px'}>
+            <Heading
+              mb={'20px'}
+              color={'#111111'}
+              fontSize={'28px'}
+              fontWeight={'bold'}
+              lineHeight={'short'}
+              letterSpacing={'-0.01em'}>
+                {title}
+            </Heading>
+            <Text mb={'8px'}>Breed:{breed}</Text>
+            <Text mb={'8px'}>Location:{location}</Text>
+            <Text mb={price && '8px'}>Age:{age}</Text>
+            {price && <Text>Price:{price}</Text>}
+          </Box>
+        </CardBody>
+        <CardFooter
+          pt={'0'}
+          px={'16px'}
+          pb={!favorite ? '32px' : '16px'}
+          width={'100%'}
+          display={'flex'}
+          flexDirection={'column'}>
+            <CardButton mb={favorite && '12px'}>
+              Learn more
+            </CardButton>
+            {favorite &&  <CardButton controle='delete'>
+                            Delete
+                          </CardButton>}
+        </CardFooter>
+    </Card>
   )
 }
 

1caf2c438e37aa7280c8ea7df9bfdd0490afe5b0
Lisnyi <russelhobbbs@gmail.com>
2023-02-01 20:04:12 +0200

    Update NoticesCategoriesList.jsx

--- src/components/NoticesCategoriesList/NoticesCategoriesList.jsx
+++ src/components/NoticesCategoriesList/NoticesCategoriesList.jsx
@@ -1,26 +1,34 @@
 import React from 'react';
+import NoticesCategoryItem from '../NoticesCategoryItem/NoticesCategoryItem'
+import { SimpleGrid } from '@chakra-ui/react';
+import {default as userPets} from './ListTest.json'
 
-import { NoticesCategoryItem } from './NoticesCategoryItem.js';
-
-const NoticesCategoriesList = userPets => {
+const NoticesCategoriesList = () => {
   // const filterPets = userPets.filter(pets => pets.condition === condition);
   return (
-    <ul key={userPets.id}>
-      {userPets.map(
-        ({ id, favorite, avatar, title, breed, location, age, condition }) => (
-          <NoticesCategoryItem
-            key={id}
-            avatar={avatar}
-            favorite={favorite}
-            title={title}
-            breed={breed}
-            location={location}
-            age={age}
-            condition={condition}
-          />
-        )
-      )}
-    </ul>
+    <SimpleGrid
+      as='ul'
+      mx={'auto'}
+      maxWidth={'280px'}
+      width={'full'}
+      listStyleType={'none'}
+      key={userPets.id}>
+        {userPets.map(
+          ({ id, favorite, avatar, title, breed, location, age, condition, price }) => (
+            <NoticesCategoryItem
+              key={id}
+              avatar={avatar}
+              favorite={favorite}
+              title={title}
+              breed={breed}
+              location={location}
+              age={age}
+              price={price}
+              condition={condition}
+            />
+          )
+        )}
+    </SimpleGrid>
   );
 };
 

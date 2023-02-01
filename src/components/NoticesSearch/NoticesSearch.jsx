f0cc159c00330046aacdff5cf259f8ba20493def
Lisnyi <russelhobbbs@gmail.com>
2023-02-01 20:03:24 +0200

    Update NoticesSearch.jsx

--- src/components/NoticesSearch/NoticesSearch.jsx
+++ src/components/NoticesSearch/NoticesSearch.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
-import { CloseIcon, Search2Icon } from '@chakra-ui/icons';
-import { Box } from '@chakra-ui/react';
+import { FilterInput } from 'shared/components';
+// import { CloseIcon, Search2Icon } from '@chakra-ui/icons';
+// import { Box } from '@chakra-ui/react';
 
 const NoticesSearch = onSubmit => {
   const [query, setQuery] = useState('');
@@ -25,29 +26,38 @@ const NoticesSearch = onSubmit => {
   };
   console.log(filteredData);
   return (
-    <Box>
-      <Box onSubmit={handleSubmit}>
-        <Box
-          type="text"
-          name="input"
-          autocomplete="off"
-          placeholder="Search"
-          onChange={handleChange}
-          value={filteredData}
-        />
-        <Box type="submit">
-          {filteredData.length === 0 ? (
-            <Search2Icon />
-          ) : (
-            <CloseIcon onClick={clearInput} />
-          )}
-        </Box>
-      </Box>
-    </Box>
+    <FilterInput
+      name={'search'}
+      onChange={handleChange}
+      handleSearch={handleSubmit} 
+      handleClear={clearInput}  
+      value={query}
+      mb={'28px'}
+    />
+    // <Box>
+    //   <Box onSubmit={handleSubmit}>
+    //     <Box
+    //       type="text"
+    //       name="input"
+    //       autocomplete="off"
+    //       placeholder="Search"
+    //       onChange={handleChange}
+    //       value={filteredData}
+    //     />
+    //     <Box type="submit">
+    //       {filteredData.length === 0 ? (
+    //         <Search2Icon />
+    //       ) : (
+    //         <CloseIcon onClick={clearInput} />
+    //       )}
+    //     </Box>
+    //   </Box>
+    // </Box>
   );
 };
-NoticesSearch.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
-};
+
+// NoticesSearch.propTypes = {
+//   onSubmit: PropTypes.func.isRequired,
+// };
 
 export default NoticesSearch;

64bebaf2c092929aab7935818330ea799999810b
Lisnyi <russelhobbbs@gmail.com>
2023-02-01 13:29:43 +0200

    rework reexports

--- src/shared/components/index.js
+++ src/shared/components/index.js
@@ -1,4 +1,5 @@
 export * from './Button';
-export { default as FormikControl } from './FormikControl/FormikControl';
-export { default as Filter } from './Filter/Filter';
-export { default as Modal } from './Modal/Modal';
+export * from './FormikControl';
+export * from './Filter';
+export * from './Modal';
+export * from './Box';

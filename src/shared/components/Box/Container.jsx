ed4ea54a213f14a427b6f6caa86b9c801cd292ad
Lisnyi <russelhobbbs@gmail.com>
2023-02-02 13:20:51 +0200

    Update Container.jsx

--- src/shared/components/Box/Container.jsx
+++ src/shared/components/Box/Container.jsx
@@ -3,7 +3,7 @@ import { Box } from '@chakra-ui/react'
 function Container({children, ...rest}) {
     return (
         <Box
-            p={'0 20px'}
+            p={{base:'0 20px', md:'0 32px', xl:'0 16px'}}
             m={'0 auto'}
             backgroundColor={'#FDF7F2'}
             {...rest}

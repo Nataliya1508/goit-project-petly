5655b428593be7cf0001a753f4a3387bcd633cb0
Velychko Daniil <101584452+Kl0filinj@users.noreply.github.com>
2023-02-03 11:43:54 +0100

    persisitor added | colors added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,8 @@
+import { Heading } from '@chakra-ui/react';
 import React from 'react';
 
 const Home = () => {
-  return <div>Home</div>;
+  return <Heading color={'accent.accent'}>Home</Heading>;
 };
 
 export default Home;

e4c76909b03a347e781187dd52c86a1c527b1947
Velychko Daniil <101584452+Kl0filinj@users.noreply.github.com>
2023-02-04 13:36:27 +0100

    header fixed

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,84 +1,100 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import { Box, Flex, HStack, Link } from '@chakra-ui/react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { Box, Flex, HStack, Link } from '@chakra-ui/react';
+import PropTypes from 'prop-types';
 
 const Nav = ({ onClick }) => {
-    const navItems = [
-        { href: '/news', text: 'News' },
-        { href: '/notices', text: 'Find pet' },
-        { href: '/friends', text: 'Our friends' }
-    ]
+  const navItems = [
+    { href: '/news', text: 'News' },
+    { href: '/notices', text: 'Find pet' },
+    { href: '/friends', text: 'Our friends' },
+  ];
 
-    return (
-        <Box
-            as='nav'
-            display={{ base: 'box', xl: 'flex' }}
-        >
-            <Flex
-                display={{ base: 'flex', xl: 'none' }}
-                flexDir='column'
-                align='center'
+  return (
+    <Box as="nav" display={{ base: 'box', xl: 'flex' }}>
+      <Flex
+        display={{ base: 'flex', xl: 'none' }}
+        flexDir="column"
+        align="center"
+      >
+        {navItems.map(({ href, text }) => (
+          <Link
+            onClick={() => onClick()}
+            to={href}
+            key={href}
+            as={NavLink}
+            mb={{ base: '40px', md: '60px' }}
+            m={{ xl: 0 }}
+            fontWeight="500"
+            fontSize={{ base: '32px', md: '48px', xl: '20px' }}
+            lineHeight={{ base: '44px', md: '66px', xl: '27px' }}
+            letterSpacing="0.04em"
+            transitionProperty={'font-weight, text-decoration, color'}
+            transitionDuration={'250ms'}
+            transitionTimingFunction={'cubic-bezier(0.4, 0, 0.2, 1)'}
+            color="#181C27"
+            _first={{ mt: { md: '34px', xl: 0 } }}
+            _last={{ mb: 0 }}
+            _hover={{
+              fontWeight: '700',
+              textDecoration: 'underline',
+              color: '#F59256',
+            }}
+            _focus={{
+              fontWeight: '700',
+              textDecoration: 'underline',
+              color: '#F59256',
+            }}
+          >
+            {text}
+          </Link>
+        ))}
+      </Flex>
+      <Flex
+        display={{ base: 'none', xl: 'flex' }}
+        align="center"
+        ml={{ xl: 20 }}
+      >
+        <HStack spacing="80px">
+          {navItems.map(({ href, text }) => (
+            <Link
+              to={href}
+              key={href}
+              as={NavLink}
+              mb={{ base: '40px', md: '60px' }}
+              m={{ xl: 0 }}
+              fontWeight="500"
+              fontSize={{ base: '32px', md: '48px', xl: '20px' }}
+              lineHeight={{ base: '44px', md: '66px', xl: '27px' }}
+              letterSpacing="0.04em"
+              transitionProperty={'font-weight, text-decoration, color'}
+              transitionDuration={'350ms'}
+              transitionTimingFunction={'cubic-bezier(0.4, 0, 0.2, 1)'}
+              color="#181C27"
+              _first={{ mt: { md: '34px', xl: 0 } }}
+              _last={{ mb: 0 }}
+              _hover={{
+                fontWeight: '700',
+                textDecoration: 'underline',
+                color: '#F59256',
+              }}
+              _focus={{
+                fontWeight: '700',
+                textDecoration: 'underline',
+                color: '#F59256',
+              }}
             >
-                {navItems.map(({ href, text }) => <Link
-                    onClick={() => onClick()}
-                    to={href}
-                    key={href}
+              {text}
+            </Link>
+          ))}
+        </HStack>
+      </Flex>
+    </Box>
+  );
+};
 
-                    as={NavLink}
-                    mb={{ base: '40px', md: '60px' }}
-                    m={{ xl: 0 }}
-                    fontWeight='500'
-                    fontSize={{ base: '32px', md: '48px', xl: '20px' }}
-                    lineHeight={{ base: '44px', md: '66px', xl: '27px' }}
-                    letterSpacing='0.04em'
-                    color='#181C27'
-
-                    _first={{ mt: { md: '34px', xl: 0 } }}
-                    _last={{ mb: 0 }}
-                    _hover={{ fontWeight: '700', textDecorationLine: 'underline', color: '#F59256' }}
-                    _focus={{ fontWeight: '700', textDecorationLine: 'underline', color: '#F59256' }}
-                >
-                    {text}
-                </Link>)}
-            </Flex>
-            <Flex
-                display={{ base: 'none', xl: 'flex' }}
-                align='center'
-                ml={{ xl: 20 }}
-            >
-                <HStack
-                    spacing='80px'
-                >
-                    {navItems.map(({ href, text }) => <Link
-                        to={href}
-                        key={href}
-
-                        as={NavLink}
-                        mb={{ base: '40px', md: '60px' }}
-                        m={{ xl: 0 }}
-                        fontWeight='500'
-                        fontSize={{ base: '32px', md: '48px', xl: '20px' }}
-                        lineHeight={{ base: '44px', md: '66px', xl: '27px' }}
-                        letterSpacing='0.04em'
-                        color='#181C27'
-
-                        _first={{ mt: { md: '34px', xl: 0 } }}
-                        _last={{ mb: 0 }}
-                        _hover={{ fontWeight: '700', textDecorationLine: 'underline', color: '#F59256' }}
-                        _focus={{ fontWeight: '700', textDecorationLine: 'underline', color: '#F59256' }}
-                    >
-                        {text}
-                    </Link>)
-                    }
-                </HStack>
-            </Flex>
-        </Box>
-    )
-}
-
-export default Nav
+export default Nav;
 
 Nav.propTypes = {
-    onClick: PropTypes.func
-}
+  onClick: PropTypes.func,
+};

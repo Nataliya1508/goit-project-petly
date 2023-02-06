0bd200063687efd3f2ff853a04b8239c967d1de0
Viktoriia <mihaleva.victoria@gmail.com>
2023-02-06 17:24:09 +0200

    added homePage and edited pictuser in register/login

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Box } from '@chakra-ui/react';
-import backgroundMobile from '../../media/bg-auth-mobile.png';
-import backgroundTablet from '../../media/bg-auth-tablet.png';
-import backgroundLaptop from '../../media/bg-auth-laptop.png';
+import { authBackgroundMobile, authBackgroundLaptop, authBackgroundTablet } from 'media';
 import LoginForm from 'components/LoginForm/LoginForm';
 
 const Login = () => {
   return (
-    <Box as='main' bgImage={{base:backgroundMobile, md:backgroundTablet, xl:backgroundLaptop }} bgPosition='bottom' bgSize='contain' bgRepeat='no-repeat' bgColor='accent.background' h='100vh' pt={{ md:'180px', xl:'60px'}}>
+    <Box as='main' bgImage={{base:authBackgroundMobile, md:authBackgroundTablet, xl:authBackgroundLaptop }} bgPosition='bottom' bgSize='contain' bgRepeat='no-repeat' bgColor='accent.background' h='100vh' pt={{ md:'180px', xl:'60px'}}>
       <LoginForm />
     </Box>
   );
--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Box } from '@chakra-ui/react';
 import RegisterForm from 'components/AuthForm/RegisterForm';
-import { backgroundLaptop, backgroundMobile, backgroundTablet } from 'media';
+import { authBackgroundMobile, authBackgroundLaptop, authBackgroundTablet } from 'media';
 
 
 const Register = () => {
       
   return (
-    <Box as='main' bgImage={{base:backgroundMobile, md:backgroundTablet, xl:backgroundLaptop }} bgPosition='bottom' bgSize='contain' bgRepeat='no-repeat' bgColor='accent.background' h='100vh' pt={{ md:'145px', xl:'26px'}}>
+    <Box as='main' bgImage={{base:authBackgroundMobile, md:authBackgroundTablet, xl:authBackgroundLaptop }} bgPosition='bottom' bgSize='contain' bgRepeat='no-repeat' bgColor='accent.background' h='100vh' pt={{ md:'145px', xl:'26px'}}>
         <RegisterForm />
     </Box>
   )

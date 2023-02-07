d68596616433846beddfb869d4df4c4d56a1770c
SerhiiIsaev <97984230+SerhiiIsaev@users.noreply.github.com>
2023-02-07 13:22:34 +0200

    fixed mistakes in console

--- src/components/UserDataItem/UserDataItem.jsx
+++ src/components/UserDataItem/UserDataItem.jsx
@@ -1,5 +1,5 @@
 import { FaPen } from 'react-icons/fa';
-import { FormControl, FormLabel, Input, Box } from '@chakra-ui/react';
+import { FormControl, FormLabel, Input, Box, Button } from '@chakra-ui/react';
 
 
 const UserDataItem = ({ nameInput, valueInput }) => {
@@ -25,16 +25,18 @@ const UserDataItem = ({ nameInput, valueInput }) => {
         fontSize={{ base: '12px', md: '18px' }}
         lineHeight={{ base: '1.35' }}
         fontWeight={{ base: '500', md: '400' }}
+        name={nameInput}
+        required="true.sting"
         px="12px"
         w={{ base: '159px', md: '220px' }}
         h={{ base: '24px', md: '32px' }}
         border="1px solid rgba(245, 146, 86, 0.5)"
         borderRadius="40px"
-        value={valueInput}
+        defaultValue={valueInput}
       />
 
       <Box
-        as="Button"
+        // as="Button"
         display='flex'
         alignItems='center'
         justifyContent='center'
